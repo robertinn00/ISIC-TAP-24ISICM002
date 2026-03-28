1aab33c7549731b274575d3c859626ba4f4a6153
Roberto Yahir Hernandez Almazan <roberto00hernamdez@gmail.com>
2026-03-28 16:54:52 -0600

    agregando actividad 2

--- Unidad_1/TAP003/src/tap003/TAP003.java
+++ Unidad_1/TAP003/src/tap003/TAP003.java
@@ -10,22 +10,26 @@ package tap003;
  */
 public class TAP003 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        int a = 5;
-        int m = 2000;
-        int c = 3;
-        int Xn = 10;
-                
+    public static void MetodoCL(int a, int m, int c, int Xn){
         for(int i = 1 ; i <=5; i++){
             int xn1 = (a * Xn + c) % m;
-            double Un = (double) xn1/m;
+            double Un = (double) xn1/(m-1);
             
             System.out.println(i + "\t\t" + xn1 + "\t" + Un);
             Xn = xn1;
         }
+    }
+    
+    
+    public static void main(String[] args) {
+        /*int a = 13;
+        int m = 64;
+        int c = 9;
+        int Xn = 11;*/
+                
+        MetodoCL(13, 64, 9, 11);
+        System.out.println(" ");
+        MetodoCL(7, 31, 4, 5);
         
         
           
